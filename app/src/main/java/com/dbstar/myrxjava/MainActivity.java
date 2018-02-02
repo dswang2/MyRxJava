b38678ae232c02f60b0826be4462e0494435dd42
wangdisheng <wangdisheng@pekon.com>
2018-02-02 15:23:48 +0800

    测试提交，第一次提交

--- app/src/main/java/com/dbstar/myrxjava/MainActivity.java
+++ app/src/main/java/com/dbstar/myrxjava/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         initView();
         // 测试提交：第一次提交
+        // 测试提交：第二次提交
     }
 
     private void initView() {
