26a98906581e1e059c577695c377200ec24e5093
wangdisheng <wangdisheng@pekon.com>
2018-02-02 15:26:51 +0800

    测试提交，第4次提交

--- app/src/main/java/com/dbstar/myrxjava/MainActivity.java
+++ app/src/main/java/com/dbstar/myrxjava/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 测试提交：第一次提交
         // 测试提交：第二次提交
         // 测试提交：第 3 次提交
+        // 测试提交：第 4 次提交
     }
 
     private void initView() {
